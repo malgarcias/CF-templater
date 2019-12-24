3fc7dfee5f81bbbc83f31205384c8c2189d1c457
Marcelo garcia soto <marcelo.algarciass@gmail.com>
2019-12-23 23:34:23 -0300

    validated with re

--- pyCfVpc.py
+++ pyCfVpc.py
@@ -3,7 +3,7 @@ import ipaddress
 import socket
 import re
 import pyVPC
-import iptools
+
 
 
 class pyCfVpc:
@@ -15,23 +15,33 @@ class pyCfVpc:
         self.incidr = incidr
         self.outcidr = outcidr
 
+def validate_cidr(cidr):
+    z = re.search('^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\/(3[0-2]|[1-2][0-9]|[0-9]))$',cidr)
+    if z:
+        return True
+    return False
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--cidr', '-c', help="VPC CIDR Address Block", type= str, required=True)
     parser.add_argument('--environment', '-e', help="Environment Name", type= str, required=True)
+    parser.add_argument('--strict', '-s', help="Strict CIDR block validation", type= bool, default=False)
     parser.add_argument('--port', '-p', help="Inbound ACL port", type= int, default=443)
     parser.add_argument('--incidr', '-i', help="Inbound ACL CIDR Block", type= str, default='0.0.0.0/0' )
     parser.add_argument('--outcidr', '-o', help="Outbound ACL CIDR Block", type= str, default='0.0.0.0/0' )
     args=parser.parse_args()
-    try:
-        iscidr = args.cidr.index('/')
-                
+    if validate_cidr(args.cidr):
+    
+        
+        
+            
         v = pyCfVpc(args.cidr, args.environment, args.port, args.incidr, args.outcidr)
         
         pyVPC.make_template(v)
-    except:
+    else:
         print('Invalid CIDR ')
 
 #
+
+    
