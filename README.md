2c0f1e6062d66529870c4636f1c7580167790c51
Marcelo garcia soto <marcelo.algarciass@gmail.com>
2019-12-24 10:18:30 -0300

    removed helper file, single file tool

--- README.md
+++ README.md
@@ -3,10 +3,7 @@
 This tool will help you create your CloudFormation VPC template with your choice of CIDR block, environment name and other custom features.
 
 
-# Files
+# Usage
 
-- pyVPC.py 					Troposphere implementation
-- pyCfVpc.py				Main class, entry point
-- README.md					This file
-- requirements.txt			you know, that file with the project dependencies ;)
-- test/test_pyCfVpc.py		Tests
+- pyCfVpc.py [-h] --cidr CIDR --environment ENVIRONMENT [--port PORT]
+                  [--incidr INCIDR] [--outcidr OUTCIDR]
--- pyCfVpc.py
+++ pyCfVpc.py
@@ -1,8 +1,15 @@
 import argparse
-import ipaddress
-import socket
 import re
-import pyVPC
+import os
+from subprocess import call
+from troposphere import (GetAtt, Output, Parameter,
+                         Ref, Tags, Template)
+from troposphere.autoscaling import Metadata
+
+from troposphere.ec2 import (VPC, Instance, InternetGateway, NetworkAcl,
+                             NetworkAclEntry, NetworkInterfaceProperty,
+                             PortRange, SecurityGroup, SecurityGroupRule,
+                             VPCGatewayAttachment)
 
 
 
@@ -22,26 +29,100 @@ def validate_cidr(cidr):
     return False
 
 
+
+
+
+def make_template(obj):
+    t = Template()
+
+    t.set_version('2010-09-09')
+
+    t.set_description("Service VPC")
+    ref_stack_id = Ref('AWS::StackId')
+    ref_region = Ref('AWS::Region')
+    ref_stack_name = Ref('AWS::StackName')
+ 
+    
+    cidr_block = obj.vpc_cidr
+    environment = obj.environment
+    port = obj.port
+    incidr = obj.incidr
+    outcidr = obj.outcidr
+
+    VPCInstance = t.add_resource(
+    VPC(
+        'VPC',
+        CidrBlock=cidr_block,
+        EnableDnsHostnames=True,
+        EnableDnsSupport=True,
+        Tags=Tags(
+            Application=ref_stack_id,
+            Name = '{}-ServiceVPC'.format(environment),
+            Environment=environment)))
+   
+    internetGateway = t.add_resource(
+        InternetGateway(
+            'InternetGateway',
+            Tags=Tags(
+                Application=ref_stack_id,
+                Environment=environment,
+                Name='{}-InternetGateway'.format(environment))))
+
+    gatewayAttachment = t.add_resource(
+        VPCGatewayAttachment(
+            'AttachGateway',
+            VpcId=Ref(VPCInstance),
+            InternetGatewayId=Ref(internetGateway)))
+
+    networkAcl = t.add_resource(
+        NetworkAcl(
+            'NetworkAcl',
+            VpcId=Ref(VPCInstance),
+            Tags=Tags(
+                Name='{}-NetworkAcl'.format(environment),
+                Environment=environment,
+                Application=ref_stack_id),
+        ))
+    vpcNetworkAclInboundRule = t.add_resource(
+        NetworkAclEntry(
+            'InboundHTTPNetworkAclEntry',
+            NetworkAclId=Ref(networkAcl),
+            RuleNumber='100',
+            Protocol='6',
+            PortRange=PortRange(To=port, From=port),
+            Egress='false',
+            RuleAction='allow',
+            CidrBlock=incidr,
+        ))
+
+    vpcNetworkAclOutboundRule= t.add_resource(
+        NetworkAclEntry(
+            'OutBoundHTTPNetworkAclEntry',
+            NetworkAclId=Ref(networkAcl),
+            RuleNumber='200',
+            Protocol='6',
+            Egress='true',
+            RuleAction='allow',
+            CidrBlock=outcidr,
+        ))
+
+    t.add_output(
+        [Output('URL',
+                Description='Newly created application URL',
+                Value='Prueba')])
+    print(t.to_json())
+#
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--cidr', '-c', help="VPC CIDR Address Block", type= str, required=True)
     parser.add_argument('--environment', '-e', help="Environment Name", type= str, required=True)
-    parser.add_argument('--strict', '-s', help="Strict CIDR block validation", type= bool, default=False)
     parser.add_argument('--port', '-p', help="Inbound ACL port", type= int, default=443)
     parser.add_argument('--incidr', '-i', help="Inbound ACL CIDR Block", type= str, default='0.0.0.0/0' )
     parser.add_argument('--outcidr', '-o', help="Outbound ACL CIDR Block", type= str, default='0.0.0.0/0' )
     args=parser.parse_args()
     if validate_cidr(args.cidr):
-    
-        
-        
-            
         v = pyCfVpc(args.cidr, args.environment, args.port, args.incidr, args.outcidr)
-        
-        pyVPC.make_template(v)
+        make_template(v)
     else:
         print('Invalid CIDR ')
-
-#
-
-    
