e2842acd295a5b47aab970f2e72b5fbe9b4a440d
Marcelo garcia soto <marcelo.algarciass@gmail.com>
2019-12-26 14:46:21 -0300

    added final test case

--- PyCfVpc.py
+++ PyCfVpc.py
@@ -1,38 +1,31 @@
 import argparse
 import re
-import os
-from subprocess import call
-from troposphere import (GetAtt, Output, Parameter,
-                         Ref, Tags, Template)
-from troposphere.autoscaling import Metadata
 
-from troposphere.ec2 import (VPC, Instance, InternetGateway, NetworkAcl,
-                             NetworkAclEntry, NetworkInterfaceProperty,
-                             PortRange, SecurityGroup, SecurityGroupRule,
+from troposphere import (Output, Ref, Tags, Template)
+
+from troposphere.ec2 import (VPC, InternetGateway, NetworkAcl,
+                             NetworkAclEntry, PortRange,
                              VPCGatewayAttachment)
 
 
+class PyCfVpc:
 
-class pyCfVpc:
-    
-    def __init__(self, vpc_cidr, environment, test, port, incidr, outcidr):
-        
+    def __init__(self, vpc_cidr, environment, port, incidr, outcidr):
         self.vpc_cidr = vpc_cidr
         self.environment = environment
-        self.test = test
         self.port = port
         self.incidr = incidr
         self.outcidr = outcidr
 
-#CIDR notation validation
+
+# CIDR notation validation
 def validate_cidr(cidr):
-    match = re.search('^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\/(3[0-2]|[1-2][0-9]|[0-9]))$',cidr)
+    match = re.search(
+        '^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\/(3[0-2]|[1-2][0-9]|[0-9]))$',
+        cidr)
     if match:
         return True
     raise ValueError('Invalid CIDR block notation')
-
-
-
 
 
 def make_template(obj):
@@ -44,26 +37,24 @@ def make_template(obj):
     ref_stack_id = Ref('AWS::StackId')
     ref_region = Ref('AWS::Region')
     ref_stack_name = Ref('AWS::StackName')
- 
-    
+
     cidr_block = obj.vpc_cidr
     environment = obj.environment
     port = obj.port
     incidr = obj.incidr
     outcidr = obj.outcidr
-    test = obj.test
 
-    VPCInstance = t.add_resource(
-    VPC(
-        'VPC',
-        CidrBlock=cidr_block,
-        EnableDnsHostnames=True,
-        EnableDnsSupport=True,
-        Tags=Tags(
-            Application=ref_stack_id,
-            Name = '{}-ServiceVPC'.format(environment),
-            Environment=environment)))
-   
+    vpcInstance = t.add_resource(
+        VPC(
+            'VPC',
+            CidrBlock=cidr_block,
+            EnableDnsHostnames=True,
+            EnableDnsSupport=True,
+            Tags=Tags(
+                Application=ref_stack_id,
+                Name='{}-ServiceVPC'.format(environment),
+                Environment=environment)))
+
     internetGateway = t.add_resource(
         InternetGateway(
             'InternetGateway',
@@ -75,13 +66,13 @@ def make_template(obj):
     gatewayAttachment = t.add_resource(
         VPCGatewayAttachment(
             'AttachGateway',
-            VpcId=Ref(VPCInstance),
+            VpcId=Ref(vpcInstance),
             InternetGatewayId=Ref(internetGateway)))
 
     networkAcl = t.add_resource(
         NetworkAcl(
             'NetworkAcl',
-            VpcId=Ref(VPCInstance),
+            VpcId=Ref(vpcInstance),
             Tags=Tags(
                 Name='{}-NetworkAcl'.format(environment),
                 Environment=environment,
@@ -99,7 +90,7 @@ def make_template(obj):
             CidrBlock=incidr,
         ))
 
-    vpcNetworkAclOutboundRule= t.add_resource(
+    vpcNetworkAclOutboundRule = t.add_resource(
         NetworkAclEntry(
             'OutBoundHTTPNetworkAclEntry',
             NetworkAclId=Ref(networkAcl),
@@ -111,33 +102,34 @@ def make_template(obj):
         ))
 
     t.add_output(
-        [Output('URL',
-                Description='Newly created application URL',
-                Value='Prueba')])
-    if obj.test:
-        return len(t.to_json().split('\n'))
-    else:
-        return t.to_json()
+        [Output('InternetGateway',
+                Description='InternetGateway',
+                Value=Ref(internetGateway)),
+         Output('VPCID',
+                Description='VPCID',
+                Value=Ref(vpcInstance))
+         ])
 
-#Entrypoint
+    return t.to_json()
+
+
+# Entrypoint
 def main():
-
-
     parser = argparse.ArgumentParser()
-    parser.add_argument('--cidr', '-c', help="VPC CIDR Address Block", type= str, required=True)
-    parser.add_argument('--environment', '-e', help="Environment Name", type= str, required=True)
-    parser.add_argument('--test', '-t', help="Dry run, test params", type= bool, default=False)
-    parser.add_argument('--port', '-p', help="Inbound ACL port", type= int, default=443)
-    parser.add_argument('--incidr', '-i', help="Inbound ACL CIDR Block", type= str, default='0.0.0.0/0' )
-    parser.add_argument('--outcidr', '-o', help="Outbound ACL CIDR Block", type= str, default='0.0.0.0/0' )
-    args=parser.parse_args()
+    parser.add_argument('--cidr', '-c', help="VPC CIDR Address Block", type=str, required=True)
+    parser.add_argument('--environment', '-e', help="Environment Name", type=str, required=True)
+    parser.add_argument('--port', '-p', help="Inbound ACL port", type=int, default=443)
+    parser.add_argument('--incidr', '-i', help="Inbound ACL CIDR Block", type=str, default='0.0.0.0/0')
+    parser.add_argument('--outcidr', '-o', help="Outbound ACL CIDR Block", type=str, default='0.0.0.0/0')
+    args = parser.parse_args()
     if validate_cidr(args.cidr):
         try:
-            v = pyCfVpc(args.cidr, args.environment, args.test, args.port, args.incidr, args.outcidr)
+            v = PyCfVpc(args.cidr, args.environment, args.port, args.incidr, args.outcidr)
             tplt = make_template(v)
             print(tplt)
         except ValueError:
             print('Invalid CIDR block notation')
-    
+
+
 if __name__ == '__main__':
     main()
--- tests/test_pyCfVPC.py
+++ tests/test_pyCfVPC.py
@@ -1,24 +1,28 @@
 import sys
-import threading
 sys.path.append('.')
 sys.path.append('..')
 
-import pyCfVpc
+import PyCfVpc
 import unittest
-import random
+
 
 class getVPCTemplate(unittest.TestCase):
     
     INVALID_CIDR = '10.12.0.256/33'
     VALID_CIDR = '10.0.0.1/16'
+    t = PyCfVpc.PyCfVpc("127.0.0.1/24","UNITTEST",443,'0.0.0.0/0','0.0.0.0/0')
 
     def testInvalidCIDRInput(self):
         """'' should raise an exception"""
-        self.assertRaises(ValueError, pyCfVpc.validate_cidr,self.INVALID_CIDR)
+        self.assertRaises(ValueError, PyCfVpc.validate_cidr, self.INVALID_CIDR)
 
     def testValidCIDRInput(self):
         """'' should assert to True"""
-        self.assertTrue(pyCfVpc.validate_cidr(self.VALID_CIDR))
+        self.assertTrue(PyCfVpc.validate_cidr(self.VALID_CIDR))
+
+    def test_main_function(self):
+        """'' should assert to Not Null"""
+        self.assertIsNotNone(PyCfVpc.make_template(self.t))
 
 if __name__ == '__main__':
     unittest.main()
